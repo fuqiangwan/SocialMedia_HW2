27b143e6a732acf4e0c6871e3a9a96e8fd3d601a
Fuqiang Wan <fuqianw@uci.edu>
2018-08-29 01:22:44 -0700

    HW update

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "K8P9Q2ETpudvCZzbXZ8O5ZCfx"
+consumer_secret = "7HwRwvc4PzeSmWo4BkRQ3WRPx09jlKqYBAIjDGiXYxhsZvVLH7"
+access_token = "766446566131531776-FsMUdm0O7jjID3R1NTVq6HtOyAhFazp"
+access_token_secret = "kOQ8ZSELey9uyJEkTq0ZIiQbN8ePQOeumHavUyWymdPBn"
